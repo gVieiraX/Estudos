a5f42854084e852e69e1dc60d825574080a5f5a4
gVieiraX <gabriel_silvarosa12@hotmail.com>
2024-08-12 17:45:38 -0300

    feat: addition, subtraction, division and multiplication

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace Calculator {
+    class Program{
+        static void Main(string[] args) {
+          Soma();
+          Subtracao();
+
+        }
+        static void Soma(){
+        Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+           
+            Console.WriteLine($"O resultado da some é: {v1+v2}.");
+            Console.ReadKey();
+
+        }
+        static void Subtracao(){
+
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 =  float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+        }
+        static void Divisao(){
+             Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 =  float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+        }
+        static void Multiplicacao(){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 =  float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.ReadKey();
+        }
+    }
+
+}
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ad60299def0a024d33a298aa49ee963b2b05ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45200e3ad80be813e08166b4cb64ebc4d43cd865")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

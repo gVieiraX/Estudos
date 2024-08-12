1ae3fb9bb8af61d45d407d7cb67579c37e0c465a
gVieiraX <gabriel_silvarosa12@hotmail.com>
2024-08-12 18:05:53 -0300

    feat: call functions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -19,6 +19,13 @@ namespace Calculator {
         Console.WriteLine("Selecione uma opção: ");
         short res = short.Parse(Console.ReadLine());
         
+        switch(res){
+            case 1: Soma(); break;
+            case 2: Subtracao();break;
+            case 3: Divisao();break;
+            case 4: Multiplicacao();break;
+            default: Menu();break;
+        }
 
        }
 
@@ -34,10 +41,10 @@ namespace Calculator {
            
             Console.WriteLine($"O resultado da some é: {v1+v2}.");
             Console.ReadKey();
+            Menu();
 
         }
         static void Subtracao(){
-
             Console.Clear();
 
             Console.WriteLine("Primeiro valor:");
@@ -49,6 +56,8 @@ namespace Calculator {
             float resultado = v1 - v2;
             Console.WriteLine($"O resultado da subtração é {resultado}");
             Console.ReadKey();
+             Menu();
+
         }
         static void Divisao(){
              Console.Clear();
@@ -75,6 +84,8 @@ namespace Calculator {
             float resultado = v1 * v2;
             Console.WriteLine($"O resultado da multiplicação é {resultado}");
             Console.ReadKey();
+            Menu();
+
         }
     }
 

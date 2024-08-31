f6c8e61e63ce8cfef5e4068200bbc201c5c632e8
gVieiraX <gabriel_silvarosa12@hotmail.com>
2024-08-31 15:35:50 -0300

    moedas complete

---
+++ Moedas/Moedas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Moedas;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        var valor = 10.25;
+        var valorMoedas = 10.6M;
+        
+        Console.WriteLine(valor);
+        Console.WriteLine(valorMoedas.ToString(
+            "C",
+            CultureInfo.CreateSpecificCulture("pt-BR")));
+        
+        Console.WriteLine(
+            Math.Round(valorMoedas)
+            );
+        
+    }
+}
